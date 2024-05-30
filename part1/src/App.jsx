f2bbe8e6c55e8289e8b094243e4dd4a17142f0fd
Yehu Carrington <yehucarrington@crimsonlogic.com>
2024-05-29 23:18:26 -0400

    Submission for Part1 Exercise1.2

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -9,17 +9,6 @@ const App = () => {
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
 
       <Header course={course}/>
       <Content part1= {part1} part2 ={part2} part3={part3} exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
@@ -40,9 +29,11 @@ const Header = (props) => {
 const Content = (props) => {
   return(
     <>
-    <p>{props.part1 + " " + props.exercises1}</p>
-    <p>{props.part2 + " " + props.exercises2}</p>
-    <p>{props.part3 + " " + props.exercises3}</p>
+
+    <Part part={props.part1} exercise={props.exercises1}/>
+    <Part part={props.part2} exercise={props.exercises2}/>
+    <Part part={props.part3} exercise={props.exercises3}/>
+
     </>
   )
 }
@@ -53,6 +44,16 @@ const Total = (props) => {
     <p>
       Number of exercises = {props.exercises1 + props.exercises2 + props.exercises3}
     </p>
+    </>
+  )
+}
+
+const Part = (props) => {
+  return(
+    <>
+      <p>
+        {props.part + " " + props.exercise}
+      </p>
     </>
   )
 }
