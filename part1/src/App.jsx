6c134a0c64a189c0b4e262598da3095435a99747
Yehu Carrington <yehucarrington@crimsonlogic.com>
2024-05-31 15:20:06 -0400

    Submission for Part1 Exercise1.6

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -8,9 +8,41 @@ const App = () => {
 
   return (
     <div>
-      code here
+      <UI/>
+      <div>
+      <Button onClick={()=>setGood(good +1)} text ="Good +1"/>
+      <Button onClick={()=>setNeutral(neutral +1)} text ="Neutral -_-"/>
+      <Button onClick={()=>setBad(bad +1)} text ="Bad +1"/>
+      </div>
+      <Stats good={good} neutral={neutral} bad={bad}/>
     </div>
   )
 }
 
 export default App
+
+const Button = ({ onClick, text }) => (
+  <div>
+    <button onClick={onClick}>{text}</button>
+  </div>
+)
+
+const UI = () => {
+  return(
+    <>
+      <h1>UniCafe Feedback Form</h1>
+    </>
+  )
+}
+
+const Stats = (props) =>{
+  return(
+    <>
+      <h2>Statistics</h2>
+      <p>{props.good}</p>
+      <p>{props.neutral}</p>
+      <p>{props.bad}</p>
+    </>
+
+  )
+}
