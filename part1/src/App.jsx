594590cf846744adb3b91d6cb4585c94cfc97dc7
Yehu Carrington <yehucarrington@crimsonlogic.com>
2024-05-31 13:39:58 -0400

    Sets the starting point of Fullstack_part1_exercise1.6

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,58 +1,15 @@
-const App = () => {
+import { useState } from 'react'
 
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
   return (
     <div>
-
-      <Header course={course}/>
-      <Content course={course}/>
-      <Total course={course}/>
-
+      code here
     </div>
-  )
-}
-
-const Header = (props) => {
-  console.log(props.course.name)
-  return(
-    <>
-    <h1>{props.course.name}</h1>
-    </>
-  )
-}
-
-const Content = (props) => {
-  return(
-    <>
-      <p>{props.course.parts[0].name} {props.course.parts[0].exercises}</p>
-      <p>{props.course.parts[1].name} {props.course.parts[1].exercises}</p>
-      <p>{props.course.parts[2].name} {props.course.parts[2].exercises}</p>
-    </>
-  )
-}
-
-const Total = (props) => {
-  return(
-    <>
-    <p>Number of exercises: {props.course.parts[0].exercises + props.course.parts[1].exercises + props.course.parts[2].exercises}</p>
-    </>
   )
 }
 
