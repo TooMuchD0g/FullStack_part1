b02b73b6de1f5663cf5082d2eabdd3396f0159c9
Yehu Carrington <yehucarrington@icloud.com>
2024-06-03 20:25:30 -0400

    Submission for part1 Exercise1.13*

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,113 +1,41 @@
 import { useState } from 'react'
 
 const App = () => {
-  // saving all clicks to a single object\
-  const [clicks, setClicks] = useState({
-    good: 0, neutral: 0, bad:0, total:0, sum: 0
-  })
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+   
+  const [selected, setSelected] = useState(0)
+  const [rank, setRank] = useState(new Uint8Array(anecdotes.length))
 
-  const handleGoodClicks = () => 
-    setClicks({...clicks, sum: clicks.sum + 1, total: clicks.total + 1, good: clicks.good + 1})
+  const handleClick = () => {
+    let number = anecdotes.length
+    let randomAnecdote = Math.floor(Math.random() * number)
+    setSelected(randomAnecdote)
+  }
 
-  const handleNeutralClicks = () =>
-    setClicks({...clicks, total: clicks.total + 1, neutral: clicks.neutral + 1})
-
-  const handleBadClicks = () =>
-    setClicks({...clicks, sum: clicks.sum - 1, total: clicks.total + 1, bad: clicks.bad + 1})
-
-  let average = clicks.sum / clicks.total;
-  let positivePercentage = (clicks.good / clicks.total) * 100;
-
+  const handleRankClick = () => {
+    const copy = [...rank]
+    copy[selected] += 1
+    setRank(copy)
+  }
 
   return (
+    console.log(rank),
     <div>
-      <UI/>
-      <Button onClick={handleGoodClicks} text ="Good +1"/>
-      <Button onClick={handleNeutralClicks} text ="Neutral -_-"/>
-      <Button onClick={handleBadClicks} text ="Bad +1"/>
-
-      <h2>Statistics</h2>
-
-      <table>
-        <tr>
-          <td>Good</td>
-          <td>{clicks.good}</td>
-        </tr>
-        <tr>
-          <td>Neutral</td>
-          <td>{clicks.neutral}</td>
-        </tr>
-        <tr>
-          <td>Bad</td>
-          <td>{clicks.bad}</td>
-        </tr>
-        <tr>
-          <td>Total</td>
-          <td>{clicks.total}</td>
-        </tr>
-        <tr>
-          <td>Average</td>
-          <td>{average}</td>
-        </tr>
-        <tr>
-          <td>Percentage</td>
-          <td>{positivePercentage}%</td>
-        </tr>
-      </table>
-
-
+      <p>{anecdotes[selected]}</p>
+      <p>Has {rank[selected]} votes</p>
+      <button onClick={handleRankClick}>vote</button>
+      <button onClick={handleClick}>next anecdote</button>
     </div>
   )
 }
 
 export default App
-
-const Button = ({ onClick, text }) => (
-  <div>
-    <button onClick={onClick}>{text}</button>
-  </div>
-)
-
-const UI = () => {
-  return(
-    <>
-      <h1>UniCafe Feedback Form</h1>
-    </>
-  )
-}
-
-const Statistics = ({ good, neutral, bad, total, sum }) => {
-  let average = sum / total;
-  let positivePercentage = (good / total) * 100;
-
-  return (
-    <>
-      <h2>Statistics</h2>
-      <p>Good: {good}</p>
-      <p>Neutral: {neutral}</p>
-      <p>Bad: {bad}</p>
-      <p>Total: {total}</p>
-      <p>Average: {average}</p>
-      <p>Positive Percentage: {positivePercentage}% </p>
-    </>
-  )
-}
-
-const StatisticsLine = (props) => {
-
-  let average = props.sum / props.total;
-  let positivePercentage = (props.good / props.total) * 100;
-
-  if(props.text == "good")
-    return <p>Good: {props.good}</p>
-  if(props.text == "neutral")
-    return <p>Neutral: {props.neutral}</p>
-  if(props.text == "bad")
-    return <p>Bad: {props.bad}</p>
-  if(props.text == "total")
-    return <p>Total: {props.total}</p>
-  if(props.text == "average")
-    return <p>Average: {average}</p>
-  if(props.text == "percentage")
-    return <p>Positive: {positivePercentage}%</p>
-}
